af43b707b24cb0ed920ecd7ffda78b7211ad91d6
soorajkumarluhana <soorajkumarluhana@gmail.com>
2020-12-31 00:04:57 +0500

    Done with Basic Calculator

---
+++ app.js
@@ -0,0 +1,15 @@
+
+function getnumber(num){
+    var result = document.getElementById("result")
+    result.value += num
+}
+
+function clearResult(){
+    var result = document.getElementById("result")
+     result.value = "";
+}
+
+function getresult(){
+    var result = document.getElementById("result")
+    result.value = eval(result.value);
+}
